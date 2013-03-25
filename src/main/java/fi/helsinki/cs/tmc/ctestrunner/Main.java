5b965fb1df8dd3314e0a350252b72df8bbca0528
Tony Kovanen <tonykovanen@hotmail.com>
2013-03-25 11:38:21 +0200

    omg

--- src/main/java/fi/helsinki/cs/tmc/ctestrunner/Main.java
+++ src/main/java/fi/helsinki/cs/tmc/ctestrunner/Main.java
@@ -32,11 +32,12 @@ public class Main {
         // Let's assume code is already compiled
         // This is done by tmc-run script which is passed to the sandbox with each submission
         try {
+            System.out.println("Reading properties");
             readProperties();
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             System.out.println();
-            System.exit(1);
+            System.exit(1337);
         }
         File memoryTestFile = new File(memoryTestOutputFilename);
         if (!memoryTestFile.exists()) memoryTestFile = null;
